2030ca727be31c6d86ae3f8c396b7d2b6ced3a00
Lovish Tater <59690614+lovishtater@users.noreply.github.com>
2020-10-04 20:40:27 +0530

    Revert "added css variables"

--- javascript30  projects/01 - JavaScript Drum Kit/03 - CSS Variables/app.js
+++
@@ -1,9 +0,0 @@
-const input = document.querySelectorAll(".controls input , .hl");
-
-function changecss (){
-    const suffix = this.dataset.sizing || `` ;
-document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-};
-
-// input.forEach(input => input.addEventListener("change", changecss));
-input.forEach(input=> input.addEventListener("mousemove",changecss));
